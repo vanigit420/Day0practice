12a4c900121b28447161268caef62ae3159c6221
shivani korwan <shivanikorwan63232@gmail.com>
2022-05-22 11:52:06 +0530

    first program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 876767;
+            double = 6.767677
+
+            Console.Writeline("Hello World!!!!!!! - "+ Value1); 
+            Console.WriteLine("Double Value - " + value2);
+          
+           
+    }
+
+}
+
+
